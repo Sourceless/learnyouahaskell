73e31ada6e969d8b1543666fe0540cf31b8198c2
Sourceless <laurence@sourceless.org>
2013-07-16 19:27:57 +0100

    Next part of the higher order functions chapter

--- 05-higher-order-functions.hs
+++ 05-higher-order-functions.hs
@@ -18,4 +18,82 @@ mutlthree :: (Num a) => a -> a -> a -> a
 multthree x y z = x * y * z
 
 --   - each argument applied in turn to each component function
---   - by calling functions with too few parameters you're 
+--   - by calling functions with too few parameters you're creating
+--     new, partial functions on the fly
+
+compareWithHundred :: (Num a, Ord a) => a -> Ordering
+compareWithHundred x = compare 100 x
+
+--   - the above can be rewritten as:
+
+compareWithHundred :: (Num a, Ord a) => a -> Ordering
+compareWithHundred = compare 100
+
+--   - infix functions can be partially applied using sections
+--   - to do this simply surround the infix function with parens
+
+divideByTen :: (Floating a) => a -> a
+divideByTen = (/10)
+
+isUpperAlphanum :: Char -> Bool
+isUpperAlphanum = (`elem` ['A'..'Z'])
+
+-- Higher Order Functions
+
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+--   - before in the type declaration, parens were not needed
+--     because -> is naturally right-associative
+--   - they're mandatory here because they indicate the first
+--     parameter is a function.
+
+zipwith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+--   - the first parameter to this function is a function that takes
+--     two paramaters of type a and b and retuns one of type c, the
+--     second and third are lists of type a and b, and returned is a list
+--     containing type c
+
+flip' :: (a -> b -> c) -> (b -> a -> c)
+flip' f = g
+    where g x y = f y x
+
+--   - takes a function a on b, returns a function b on a
+--   - the second set of parentheses aren't needed as -> is right-associative
+
+flip'' :: (a -> b -> c) -> b -> a -> c
+flip'' f y x = f x y
+
+-- Maps and Filters
+--   - map takes a function and a list and applies the function to every item
+--     of the list
+
+map'' :: (a -> b) -> [a] -> [b]
+map'' _ [] = []
+map'' f (x:xs) = f x : map f xs
+
+--  - these can also be achieved using a list comprehension
+--  - map is more readable for cases when you only apply a function to a list
+--  - filter takes a predicate and a list and returns a list where all the
+--    items satisfy the predicate
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter p (x:xs)
+    | p x       = x : filter p xs
+    | otherwise = filter p xs
+
+--  - quicksort can be implemented more simply with filter
+
+quicksort :: (Ord a_ => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+    let smallerSorted = quicksort (filter (<=x) xs)
+        biggerSorted = quicksort (filter (>x) xs)
+    in smallerSorted ++ [x] ++ biggerSorted
+
+    -- to be continued!
